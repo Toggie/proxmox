2ae650755f0acb7a9328b41104af6a6208da9e42
Jeff Johnston <jeff@MacBook-Pro-2.local>
2016-03-28 01:39:49 -0400

    added lxc support

--- lib/proxmox.rb
+++ lib/proxmox.rb
@@ -118,13 +118,13 @@ module Proxmox
     # Get CT list
     #
     # :call-seq:
-    #   openvz_get -> Hash
+    #   lxc_get -> Hash
     #
-    # Return a Hash of all openvz container
+    # Return a Hash of all lxc container
     #
     # Example:
     #
-    #   openvz_get
+    #   lxc_get
     #
     # Example return:
     #   {
@@ -147,13 +147,13 @@ module Proxmox
     #           'failcnt' => 0,
     #           'diskwrite' => 0,
     #           'mem' => 22487040,
-    #           'type' => 'openvz',
+    #           'type' => 'lxc',
     #           'cpus' => 1
     #     },
     #     [...]
     #   }
-    def openvz_get
-      data = http_action_get "nodes/#{@node}/openvz"
+    def lxc_get
+      data = http_action_get "nodes/#{@node}/lxc"
       ve_list = {}
       data.each do |ve|
         ve_list[ve['vmid']] = ve
@@ -164,133 +164,133 @@ module Proxmox
     # Create CT container
     #
     # :call-seq:
-    #   openvz_post(ostemplate, vmid) -> String
-    #   openvz_post(ostemplate, vmid, options) -> String
+    #   lxc_post(ostemplate, vmid) -> String
+    #   lxc_post(ostemplate, vmid, options) -> String
     #
     # Return a String as task ID
     #
     # Examples:
     #
-    #   openvz_post('ubuntu-10.04-standard_10.04-4_i386', 200)
-    #   openvz_post('ubuntu-10.04-standard_10.04-4_i386', 200, {'hostname' => 'test.test.com', 'password' => 'testt' })
+    #   lxc_post('ubuntu-10.04-standard_10.04-4_i386', 200)
+    #   lxc_post('ubuntu-10.04-standard_10.04-4_i386', 200, {'hostname' => 'test.test.com', 'password' => 'testt' })
     #
     # Example return:
     #
     #   UPID:localhost:000BC66A:1279E395:521EFC4E:vzcreate:200:root@pam:
     #
-    def openvz_post(ostemplate, vmid, config = {})
+    def lxc_post(ostemplate, vmid, config = {})
       config['vmid'] = vmid
       config['ostemplate'] = "local%3Avztmpl%2F#{ostemplate}.tar.gz"
       vm_definition = config.to_a.map { |v| v.join '=' }.join '&'
 
-      http_action_post("nodes/#{@node}/openvz", vm_definition)
+      http_action_post("nodes/#{@node}/lxc", vm_definition)
     end
 
     # Delete CT
     #
     # :call-seq:
-    #   openvz_delete(vmid) -> String
+    #   lxc_delete(vmid) -> String
     #
     # Return a string as task ID
     #
     # Example:
     #
-    #   openvz_delete(200)
+    #   lxc_delete(200)
     #
     # Example return:
     #
     #   UPID:localhost:000BC66A:1279E395:521EFC4E:vzdelete:200:root@pam:
     #
-    def openvz_delete(vmid)
-      http_action_delete "nodes/#{@node}/openvz/#{vmid}"
+    def lxc_delete(vmid)
+      http_action_delete "nodes/#{@node}/lxc/#{vmid}"
     end
 
     # Get CT status
     #
     # :call-seq:
-    #   openvz_delete(vmid) -> String
+    #   lxc_delete(vmid) -> String
     #
     # Return a string as task ID
     #
     # Example:
     #
-    #   openvz_delete(200)
+    #   lxc_delete(200)
     #
     # Example return:
     #
     #   UPID:localhost:000BC66A:1279E395:521EFC4E:vzdelete:200:root@pam:
     #
-    def openvz_status(vmid)
-      http_action_get "nodes/#{@node}/openvz/#{vmid}/status/current"
+    def lxc_status(vmid)
+      http_action_get "nodes/#{@node}/lxc/#{vmid}/status/current"
     end
 
     # Start CT
     #
     # :call-seq:
-    #   openvz_start(vmid) -> String
+    #   lxc_start(vmid) -> String
     #
     # Return a string as task ID
     #
     # Example:
     #
-    #   openvz_start(200)
+    #   lxc_start(200)
     #
     # Example return:
     #
     #   UPID:localhost:000BC66A:1279E395:521EFC4E:vzstart:200:root@pam:
     #
-    def openvz_start(vmid)
-      http_action_post "nodes/#{@node}/openvz/#{vmid}/status/start"
+    def lxc_start(vmid)
+      http_action_post "nodes/#{@node}/lxc/#{vmid}/status/start"
     end
 
     # Stop CT
     #
     # :call-seq:
-    #   openvz_stop(vmid) -> String
+    #   lxc_stop(vmid) -> String
     #
     # Return a string as task ID
     #
     # Example:
     #
-    #   openvz_stop(200)
+    #   lxc_stop(200)
     #
     # Example return:
     #
     #   UPID:localhost:000BC66A:1279E395:521EFC4E:vzstop:200:root@pam:
     #
-    def openvz_stop(vmid)
-      http_action_post "nodes/#{@node}/openvz/#{vmid}/status/stop"
+    def lxc_stop(vmid)
+      http_action_post "nodes/#{@node}/lxc/#{vmid}/status/stop"
     end
 
     # Shutdown CT
     #
     # :call-seq:
-    #   openvz_shutdown(vmid) -> String
+    #   lxc_shutdown(vmid) -> String
     #
     # Return a string as task ID
     #
     # Example:
     #
-    #   openvz_shutdown(200)
+    #   lxc_shutdown(200)
     #
     # Example return:
     #
     #   UPID:localhost:000BC66A:1279E395:521EFC4E:vzshutdown:200:root@pam:
     #
-    def openvz_shutdown(vmid)
-      http_action_post "nodes/#{@node}/openvz/#{vmid}/status/shutdown"
+    def lxc_shutdown(vmid)
+      http_action_post "nodes/#{@node}/lxc/#{vmid}/status/shutdown"
     end
 
     # Get CT config
     #
     # :call-seq:
-    #   openvz_config(vmid) -> String
+    #   lxc_config(vmid) -> String
     #
     # Return a string as task ID
     #
     # Example:
     #
-    #   openvz_config(200)
+    #   lxc_config(200)
     #
     # Example return:
     #
@@ -311,27 +311,27 @@ module Proxmox
     #     'storage' => 'local'
     #   }
     #
-    def openvz_config(vmid)
-      http_action_get "nodes/#{@node}/openvz/#{vmid}/config"
+    def lxc_config(vmid)
+      http_action_get "nodes/#{@node}/lxc/#{vmid}/config"
     end
 
     # Set CT config
     #
     # :call-seq:
-    #   openvz_config_set(vmid, parameters) -> Nil
+    #   lxc_config_set(vmid, parameters) -> Nil
     #
     # Return nil
     #
     # Example:
     #
-    #   openvz_config(200, { 'swap' => 2048 })
+    #   lxc_config(200, { 'swap' => 2048 })
     #
     # Example return:
     #
     #   nil
     #
-    def openvz_config_set(vmid, data)
-      http_action_put("nodes/#{@node}/openvz/#{vmid}/config", data)
+    def lxc_config_set(vmid, data)
+      http_action_put("nodes/#{@node}/lxc/#{vmid}/config", data)
     end
 
     private
